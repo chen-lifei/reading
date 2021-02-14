f9a02f85c7d24deb93d315826be0d322f39b549a
chen-lifei <feilichen@yeah.net>
2021-02-14 14:49:01 +0800

    feat: 名著页面推荐模块布局

--- src/views/masterpiece/index.vue
+++ src/views/masterpiece/index.vue
@@ -2,15 +2,30 @@
     <div class="masterpieceContent">
         <div class="homeContent" v-if="isHome">
             <div class="topContent">
-                <img src="@/assets/masterpiece/bg3.png" class="title" />
-                <img class="banner" src="@/assets/masterpiece/1.png" />
+                <img src="@/assets/masterpiece/title.png" class="title" />
+                <img class="banner" src="@/assets/masterpiece/bg.png" />
                 <div class="secondNav">
                     <ul>
                         <li v-for="(item, index) in navList" :key="index" @click="changeNav(item.id)">
-                            <img src="@/assets/masterpiece/bg2.png" />
+                            <img src="@/assets/masterpiece/navBg.png" />
                             <span class="text">{{item.label}}</span>
                         </li>
                     </ul>
+                </div>
+            </div>
+            <div class="recommend">
+                <div class="title">推荐专区</div>
+                <div class="change">换一换<i class="el-icon-refresh-right" style="padding-left: 5px"></i></div>
+                <div class="recommendContent mt20">
+                    <div class="contentBox" v-for="(item, index) in books" :key="index">
+                        <img :src="item.img" />
+                        <div class="detail">{{item.detail}}</div>
+                        <div class="info">
+                            <div class="name">{{item.name}}</div>
+                            <div class="author">{{item.author}}</div>
+                        </div>
+                        <div class="read">进行阅读</div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -32,45 +47,35 @@ export default {
             books: [
                 // detail限制50个字
                 {
-                    img: require('@/assets/hua.png'),
+                    img: require('@/assets/elk.png'),
                     name: '追风筝的人',
                     author: '不知道',
                     detail:
                         '时雍上辈子为了男人肝脑涂地，最后得了个“女魔头”的恶名惨死诏狱，这才明白穿越必有爱情是个笑话。重生到阿拾身上，她决定做个平平无奇的女差役混吃等死。可从此以后，锦衣卫大都督靠...'
                 },
                 {
-                    img: require('@/assets/hua.png'),
+                    img: require('@/assets/baiyexing.jpg'),
+                    name: '追风筝的人',
+                    author: '不知道',
+                    detail:
+                        '时雍上辈子为了男人肝脑涂地，最后得了个“女魔头”的恶名惨死诏狱，这才明白穿越必有爱情是个笑话。重生到阿拾身上，她决定做个平平无奇的女差役混吃等死。可从此以后，锦衣卫大都督靠时雍上辈子为了男人肝脑涂地，最后得了个“女魔头”的恶名惨死诏狱，这才明白穿越必有爱情是个笑话。重生到阿拾身上，她决定做个平平无奇的女差役混吃等死。可从此以后，锦衣卫大都督靠时雍上辈子为了男人肝脑涂地，最后得了个“女魔头”的恶名惨死诏狱，这才明白穿越必有爱情是个笑话。重生到阿拾身上，她决定做个平平无奇的女差役混吃等死。可从此以后，锦衣卫大都督靠...'
+                },
+                {
+                    img: require('@/assets/elk.png'),
                     name: '追风筝的人',
                     author: '不知道',
                     detail:
                         '时雍上辈子为了男人肝脑涂地，最后得了个“女魔头”的恶名惨死诏狱，这才明白穿越必有爱情是个笑话。重生到阿拾身上，她决定做个平平无奇的女差役混吃等死。可从此以后，锦衣卫大都督靠...'
-                },
-                {
-                    img: require('@/assets/hua.png'),
-                    name: '追风筝的人',
-                    author: '不知道',
-                    detail:
-                        '时雍上辈子为了男人肝脑涂地，最后得了个“女魔头”的恶名惨死诏狱，这才明白穿越必有爱情是个笑话。重生到阿拾身上，她决定做个平平无奇的女差役混吃等死。可从此以后，锦衣卫大都督靠...'
-                },
-                {
-                    img: require('@/assets/hua.png'),
-                    name: '追风筝的人',
-                    author: '不知道',
-                    detail:
-                        '时雍上辈子为了男人肝脑涂地，最后得了个“女魔头”的恶名惨死诏狱，这才明白穿越必有爱情是个笑话。重生到阿拾身上，她决定做个平平无奇的女差役混吃等死。可从此以后，锦衣卫大都督靠...'
-                },
-                {
-                    img: require('@/assets/hua.png'),
-                    name: 'Guess me',
-                    author: 'hahaah',
-                    detail:
-                        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text.'
                 }
             ],
             isHome: true
         }
     },
-    watch: {},
+    watch: {
+        $route () {
+            this.getHome()
+        }
+    },
     computed: {},
     methods: {
         changeNav (category) {
